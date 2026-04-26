630e8c5fe2dbeef6eed174d8010608c1b090bbd9
AndrewTSipes <165952815+AndrewTSipes@users.noreply.github.com>
2026-04-25 20:18:15 -0400

    Implement copy assignment operator in Message class
    Added copy assignment operator for Message class and included cstring header.

--- Phase 3/IPC.h
+++ Phase 3/IPC.h
@@ -3,21 +3,19 @@
 
 #include <time.h>
 #include <string>
+#include <cstring>
 #include "Sema.h"
 #include "Queue.h"
 
-//Message Type struct
 struct Message_Type {
     int Message_Type_Id;
     char Message_Type_Description[64];
 };
 
-//Message type constants, will be added to if more message types become necessary
 const Message_Type MESSAGE_TYPE_TEXT {0, "Text message, no response"};
 const Message_Type MESSAGE_TYPE_SERVICE_REQUEST {1, "Request for service, send service notification back"};
 const Message_Type MESSAGE_TYPE_SERVICE_NOTIFICATION {2, "Notification of service request result, no response"};
 
-//Message struct
 struct Message {
     int Source_Task_Id;
     int Destination_Task_Id;
@@ -28,28 +26,43 @@ struct Message {
 
     Message();
     Message(int s_id, int d_id, Message_Type msg_t, char *msg);
-
-    // FIX: must take const Message& so it can copy temporaries
     Message(const Message &msg);
+
+    Message& operator=(const Message &msg) {
+        if (this == &msg) return *this;
+
+        Source_Task_Id = msg.Source_Task_Id;
+        Destination_Task_Id = msg.Destination_Task_Id;
+        Message_Arrival_Time = msg.Message_Arrival_Time;
+        Msg_Type = msg.Msg_Type;
+        Msg_Size = msg.Msg_Size;
+
+        delete[] Msg_Text;
+
+        if (msg.Msg_Text && Msg_Size > 0) {
+            Msg_Text = new char[Msg_Size];
+            memcpy(Msg_Text, msg.Msg_Text, Msg_Size);
+        } else {
+            Msg_Text = nullptr;
+        }
+
+        return *this;
+    }
 
     std::string print();
     ~Message();
 };
 
-//Inter Process Communication class.
-//Holds and manages the mailboxes for every task.
-//Assumes that task id and position in the mailbox array are the same value for every task.
 class ipc {
 private:
-    class Mailbox : public Queue<Message>
-    {
+    class Mailbox : public Queue<Message> {
         private:
         semaphore sema;
         friend class ipc;
         public:
         Mailbox();
         Mailbox(scheduler *sched, std::string name);
-        void print_all(); 
+        void print_all();
     };
 
     Mailbox *mailbox;
